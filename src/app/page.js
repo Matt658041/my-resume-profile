3abcc56d79880688cfadaf608499a11213b8d7c9
Matthew Boisse <matthewboisse@Matthews-MacBook-Pro.local>
2025-02-15 11:12:42 -0600

    images need formatting

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,24 @@
 import Image from "next/image";
 import About from "../../components/About";
-import { Grid } from "@mui/material/Grid2";
+import Skills from "../../components/Skills";
+import {Grid2} from "@mui/material";
 import { Box, Container, Typography, Paper } from "@mui/material";
 
 export default function Home() {
   return (
     <>
-      <Container  sx={{ marginTop: 5, marginBottom: 1 }}>
-      
-            <About />
-       
+      <Container sx={{ marginTop: 5, marginBottom: 1 }}>
+        <Grid2
+          container
+          spacing={1} // Reduced spacing
+          sx={{
+            justifyContent: "center",
+            alignItems: "center",
+          }} // Centering components
+        >
+          <About />
+          <Skills />
+        </Grid2>
       </Container>
     </>
   );
