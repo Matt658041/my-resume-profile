5679bc9becff3645c75aef1a1f6eef6c2d13fe43
Matthew Boisse <matthewboisse@Matthews-MacBook-Pro.local>
2025-02-18 12:58:08 -0600

    getting better

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,36 +5,32 @@ import navImage from "../public/github-profile.jpg"; // Replace with your nav im
 
 const Navbar = () => {
   return (
-    <div className="fixed w-full h-20 shadow-xl z-[100] bg-white/70 backdrop-blur">
+    <div className="fixed w-full h-20 shadow-xl z-[100] bg-white/70 ">
       <div className="flex justify-between items-center w-full h-full px-4 2xl:px-16">
         <div className="flex items-center space-x-4 py-5">
-          <Link href="/">
-            <Image
-              className="rounded-lg"
-              src={navImage}
-              alt="/"
-              width="70"
-              height="80"
-            />
+          <Link href="/"></Link>
+          <p className="text-lg ml-4">Let's build something together</p>
+        </div>
+
+        <ul className="hidden md:flex space-x-5">
+          <Link href="/#about">
+            <li className=" mt-2 ml-10 text-lg uppercase hover:border-b">About</li>
           </Link>
-          <p className="text-lg ml-4">
-            Let's build something legendary together
-          </p>
-        </div>
-        <div className="flex-row space-x-4 hidden md:flex">
-          <div className="text-lg uppercase hover:border-b">
-            <Link href="/#about">About</Link>
-          </div>
-          <div className="text-lg uppercase hover:border-b ">
-            <Link href="/#skills">Skills</Link>
-          </div>
-          <div className="text-lg uppercase hover:border-b">
-            <Link href="/#projects">Projects</Link>
-          </div>
-          <div className="text-lg uppercase hover:border-b pr-10">
-            <Link href="/#contact">Contact</Link>
-          </div>
-        </div>
+
+          <Link href="/#skills">
+            <li className=" ml-10 text-lg uppercase hover:border-b">Skills</li>
+          </Link>
+
+          <Link href="/#projects">
+            <li className=" ml-10 text-lg uppercase hover:border-b">
+              Projects
+            </li>
+          </Link>
+
+          <Link href="/#contact">
+            <li className=" ml-10 text-lg uppercase hover:border-b">Contact</li>
+          </Link>
+        </ul>
       </div>
     </div>
   );
