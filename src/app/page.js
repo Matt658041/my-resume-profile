61b8a85d6a468ce718583d52683530aa7eac345f
Matthew Boisse <matthewboisse@Matthews-MacBook-Pro.local>
2025-02-18 09:44:12 -0600

    corrected typo in href

--- src/app/page.js
+++ src/app/page.js
@@ -308,7 +308,7 @@ export default function Home() {
         <div className=" rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
           <Link href="https://www.linkedin.com/in/matthew-boisse-b3a296224/">
             <FaLinkedinIn
-              a
+              
               href="www.linkedin.com/in/matthew-boisse-b3a296224"
             />
           </Link>
