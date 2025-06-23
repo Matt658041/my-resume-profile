cac777498ab5eca5ff74b8eae039453b0b665038
Matthew Boisse <matthewboisse@Matthews-MacBook-Pro.local>
2025-06-23 11:11:35 -0500

    hope this works

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,31 +5,34 @@ import navImage from "../public/github-profile.jpg"; // Replace with your nav im
 
 const Navbar = () => {
   return (
-    <div className="fixed w-full h-20 shadow-xl z-[100] bg-white/70 ">
-      <div className="flex justify-between items-center w-full h-full px-4 2xl:px-16">
-        <div className="flex items-center space-x-4 py-5">
-          <Link href="/"></Link>
-          <p className="text-xl ml-4">Let's build something together</p>
+    <div className="fixed w-full h-20 shadow-xl z-[100] bg-white/70">
+      <div className="relative flex items-center w-full h-full px-4 2xl:px-16">
+        {/* Logo on the left */}
+        <div className="flex items-center">
+          <Link href="/">
+            <Image
+              className="rounded-lg"
+              src={navImage}
+              alt="Logo"
+              width={60}
+              height={60}
+            />
+          </Link>
         </div>
-
-        <ul className="hidden md:flex space-x-5">
-          <Link href="/#about">
-            <li className=" mt-2 ml-10 text-lg uppercase hover:border-b">About</li>
-          </Link>
-
-          <Link href="/#skills">
-            <li className=" ml-10 text-lg uppercase hover:border-b">Skills</li>
-          </Link>
-
-          <Link href="/#projects">
-            <li className=" ml-10 text-lg uppercase hover:border-b">
-              Projects
-            </li>
-          </Link>
-
-          <Link href="/#contact">
-            <li className=" ml-10 text-lg uppercase hover:border-b">Contact</li>
-          </Link>
+        {/* Centered nav links */}
+        <ul className="hidden md:flex space-x-10 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <li className="text-lg uppercase hover:border-b">
+            <Link href="/#about">About</Link>
+          </li>
+          <li className="text-lg uppercase hover:border-b">
+            <Link href="/#skills">Skills</Link>
+          </li>
+          <li className="text-lg uppercase hover:border-b">
+            <Link href="/#projects">Projects</Link>
+          </li>
+          <li className="text-lg uppercase hover:border-b">
+            <Link href="/#contact">Contact</Link>
+          </li>
         </ul>
       </div>
     </div>
